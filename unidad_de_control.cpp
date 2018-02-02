6caf368059f466804dbb00b628934a541ca122f2
Jesus <jesus@Torcat>
2018-02-02 07:06:40 -0400

    Unidad de Control (Creación de las clases)

--- unidad_de_control.cpp
+++ unidad_de_control.cpp
@@ -1,3 +1,27 @@
 /** Unidad de control **/
 
 #include "unidad_de_control.h"
+
+UNIDAD_DE_CONTROL::UNIDAD_DE_CONTROL(){}
+
+UNIDAD_DE_CONTROL::~UNIDAD_DE_CONTROL(){}
+
+/**
+*
+*/
+void UNIDAD_DE_CONTROL::cargar_ri(long _ri){
+    ri = _ri;
+}
+
+void UNIDAD_DE_CONTROL::cargar_temporizador(long int _temporizador){
+    temporizador = _temporizador;
+}
+
+void UNIDAD_DE_CONTROL::cargar_indicadores(int _indicadores){
+    indicadores = _indicadores;
+
+    z = (_indicadores&0x8)>>3;
+    d = (_indicadores&0x4)>>2;
+    p = (_indicadores&0x2)>>1;
+    s = (_indicadores&0x1);
+}
--- unidad_de_control.h
+++ unidad_de_control.h
@@ -1,4 +1,24 @@
 #ifndef UNIDAD_DE_CONTROL_H
 #define UNIDAD_DE_CONTROL_H
 
+#include <iostream>
+
+class UNIDAD_DE_CONTROL
+{
+public:
+    UNIDAD_DE_CONTROL();
+    ~UNIDAD_DE_CONTROL();
+    void ejecutar_control(int&,int&,int&,int&,int&,int&,int&,int&,int&,int&,
+                          int&,int&,int&,int&,int&,int&,int&,int&,int&,int&,
+                          int&,int&,int&,int&);
+    void cargar_temporizador(long int);
+    void cargar_ri(long);
+    void cargar_indicadores(int);
+
+private:
+    long int temporizador;
+    long ri;
+    int z, d, p, s, op, indicadores;
+};
+
 #endif // UNIDAD_DE_CONTROL_H
